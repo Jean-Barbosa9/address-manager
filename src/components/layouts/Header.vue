a209ecdf80904444175cd7ae1063a5e4cf6d91f0
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-01 17:16:34 -0300

    Incluindo o header da aplicação com o logo e o título

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <img class="logo" alt="Vue logo" src="../assets/logo.png">
+    <h1 class="title">Gerenciador de Endereços</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  width: 100vw;
+  height: 50px;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  background: rgba(173, 228, 204, 0.7);
+}
+</style>
