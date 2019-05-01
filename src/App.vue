544c8ea8e83b2fecca303f9b09b4da88b1f5ec87
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-01 17:12:36 -0300

    Iniciando a aplicação com a importação dos componentes e estilos globais

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header/>
+    <NewAddress/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Components importings
+import Header from "./components/Header";
+import NewAddress from "./components/NewAddress";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    NewAddress
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  /* box-sizing: border-box; */
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Open Sans", Arial, Helvetica, sans-serif, -apple-system,
+    BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  line-height: 1.4;
+}
+
+.logo {
+  max-width: 50px;
+}
+
+.title {
+  font-size: 1em;
+}
+
+.form {
+  width: 70vw;
+  margin: 40px auto 0 auto;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 20px;
+}
+
+@media screen and (min-width: 1025px) {
+  .title {
+    font-size: 1.5em;
+  }
+
+  .form {
+    width: 50vw;
+  }
 }
 </style>
