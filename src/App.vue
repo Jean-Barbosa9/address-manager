597a96f47a70652256d9d24e19b7e97a567322f5
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-03 00:58:27 -0300

    criados os eventListeners para editar e deletar, método delete criado, importação do bootstrap, alteração dos endereços de exemplo e tratando o id quando não há nenhum endereço cadastrado

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <Header/>
     <div class="container">
       <NewAddress v-on:new-address="addNewAddress"/>
-      <Addresses v-bind:addresses="addresses"/>
+      <Addresses
+        v-bind:addresses="addresses"
+        v-on:edit-address="editAddress"
+        v-on:delete-address="deleteAddress"
+      />
     </div>
   </div>
 </template>
@@ -13,6 +17,9 @@
 import Header from "./components/layouts/Header";
 import NewAddress from "./components/NewAddress";
 import Addresses from "./components/Addresses";
+
+// Using bootstrap style
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 export default {
   name: "app",
@@ -25,34 +32,37 @@ export default {
     return {
       addresses: [
         {
-          id: 1,
-          zipCode: "20231050",
+          id: 3,
+          contact: "Shopping Rio Sul",
+          zipCode: "22290070",
           city: "Rio de Janeiro",
           state: "RJ",
-          street: "Rua André Cavalcanti",
-          neighborhood: "Centro",
-          number: "171",
-          complement: "Sobrado"
+          street: "Avenida Lauro Sodré",
+          neighborhood: "Botafogo",
+          number: "445",
+          complement: "Parte DC01, Shop 401"
         },
         {
           id: 2,
-          zipCode: "01001000",
-          city: "São Paulo",
-          state: "SP",
-          street: "Avenida Paulista",
-          neighborhood: "Centro",
-          number: "4000",
-          complement: "Bloco 1 Sl 302"
-        },
-        {
-          id: 3,
-          zipCode: "20535220",
+          contact: "Shopping Tijuca",
+          zipCode: "20511000",
           city: "Rio de Janeiro",
           state: "RJ",
-          street: "Rua Jair Caldas Corrêa",
-          neighborhood: "Alto da Boa Vista",
-          number: "5",
-          complement: "Casa"
+          street: "Avenida Maracanã",
+          neighborhood: "Tijuca",
+          number: "987",
+          complement: "7º piso"
+        },
+        {
+          id: 1,
+          contact: "Shopping Downtown",
+          zipCode: "22640904",
+          city: "Rio de Janeiro",
+          state: "RJ",
+          street: "Avenida das Américas",
+          neighborhood: "Centro",
+          number: "500",
+          complement: "Bloco 6 2º piso"
         }
       ]
     };
@@ -62,10 +72,16 @@ export default {
       // Verificar se o CEP que está sendo inserido já existe. Caso positivo, retornar mensagem perguntando se deseja atualizar o endereço existente
 
       // Instalar uuid para melhorar a criação de ids
-      const newId = this.addresses[this.addresses.length - 1].id++;
+      const newId = this.addresses.length > 0 ? this.addresses[0].id + 1 : 1;
       address.id = newId;
-
       this.addresses.unshift(address);
+    },
+    editAddress(id) {
+      console.log(`criar interação na tela para edtitar o item ${id}`);
+    },
+    deleteAddress(id) {
+      console.log(this.addresses);
+      this.addresses = this.addresses.filter(address => address.id !== id);
     }
   }
 };
@@ -86,34 +102,61 @@ body {
 }
 
 .logo {
-  max-width: 50px;
+  max-width: 40px;
 }
 
-.title {
+.card-header {
+  text-align: center;
+  font-weight: bold;
+}
+
+.app-title {
+  margin: 0;
+  padding: 0 10px;
+  font-family: "Wendy One", Arial, Helvetica, sans-serif, -apple-system,
+    BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: 1em;
+  text-align: center;
+  text-transform: uppercase;
+  flex: 0 1 60%;
+}
+
+.title-1 {
+  font-size: 1.2em;
+}
+
+.title-2 {
   font-size: 1em;
 }
 
-.form {
-  width: 70vw;
-  margin: 40px auto 0 auto;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 20px;
+.title-3 {
+  font-size: 0.8em;
 }
 
-.form__group--flex {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+@media screen and (min-width: 440px) {
+  .app-title {
+    font-size: 1.5em;
+    flex-basis: 80%;
+  }
 }
 
 @media screen and (min-width: 1025px) {
-  .title {
+  .app-title {
+    font-size: 2.5em;
+    flex-basis: 100%;
+  }
+
+  .title-1 {
+    font-size: 2em;
+  }
+
+  .title-2 {
     font-size: 1.5em;
   }
 
-  .form {
-    width: 50vw;
+  .title-3 {
+    font-size: 1em;
   }
 }
 </style>
