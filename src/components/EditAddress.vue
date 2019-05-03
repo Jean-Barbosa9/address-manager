a987703c3777b27520e522e125e4d6d8e6bb56e4
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-03 16:23:30 -0300

    Criando o componente para a ação de editar o endereço

---
+++ src/components/EditAddress.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="add-address card my-5">
+    <h3 class="card-header title-2">Editando {{address.title}}</h3>
+    <AddressForm
+      class="card-body"
+      usage="edit-address"
+      v-bind:address="address"
+      v-on:edit-address="editAddress"
+    />
+  </div>
+</template>
+
+<script>
+import AddressForm from "./AddressForm";
+
+export default {
+  name: "EditAddress",
+  props: ["address"],
+  components: {
+    AddressForm
+  },
+  methods: {
+    editAddress(address) {
+      this.$emit("edit-address", address);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
