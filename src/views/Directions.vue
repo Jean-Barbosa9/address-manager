fe04fe9920a823a64e09ddbd54e33aa68e578807
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-10 01:12:48 -0300

    Iniciando estrutura que a pagina tera e simulando visualizaçao de imagem

--- src/views/Directions.vue
+++ src/views/Directions.vue
@@ -1,14 +1,44 @@
 <template>
   <div>
-    <h1
-      class="title-1"
-    >Aqui será criado o componente que renderizará o mapa com a rota da localização atual do usuário para o endereço selecionado.</h1>
+    <h1 class="title-1">
+      Aqui será criado o componente que renderizará o mapa com a rota da localização atual do usuário para o endereço
+      <strong>{{this.address.title}}</strong>
+    </h1>
+    <p>
+      <strong>Rota para:</strong>
+      {{this.address.street}}, {{this.address.number}} - {{this.address.city}} / {{this.address.state}}
+    </p>
+    <div id="map">
+      <img v-bind:src="renderResponsiveImage()">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Directions"
+  name: "Directions",
+  data() {
+    return {
+      id: this.$router.history.current.params.id,
+      address: {},
+      imgSrc: ""
+    };
+  },
+  created() {
+    this.getAddress();
+  },
+  methods: {
+    getAddress() {
+      const addresses = JSON.parse(atob(localStorage.addresses));
+      this.address = addresses.filter(address => address.id == this.id)[0];
+    },
+    renderResponsiveImage() {
+      // apenas para simulação enquanto não faço o mapa de fato
+      const width = document.documentElement.clientWidth,
+        height = document.documentElement.clientHeight;
+      return `https://via.placeholder.com/${width}x${height}`;
+    }
+  }
 };
 </script>
 
