88c941255fe96acb0afa245907eb8d09cac9ce52
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-10 23:30:51 -0300

    Ajustes finos para melhorar o layout dos botoes e ja preparando botao para compartilhar endereço

--- src/components/Addresses.vue
+++ src/components/Addresses.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="addresses card" v-bind:addresses="addresses" v-if="this.addresses.length > 0">
-    <h4 class="card-header title-2">Endereços salvos</h4>
+    <div class="card-header">
+      <h4 class="title-2 mb-0 d-inline-block">Endereços salvos</h4>
+      <button
+        type="button"
+        class="d-none btn btn-darker px-3 py-2 float-right"
+        title="Compartilhar lista de endereços"
+      >
+        <i class="fas fa-share-alt"></i>
+      </button>
+    </div>
     <div class="card-body row">
       <div class="address col-sm" v-for="address in addresses" :key="address.id">
         <AddressItem
