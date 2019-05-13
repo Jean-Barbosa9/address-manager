f6d4925c81a3d93cd3a0bd49faabc81a9a19c66f
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-13 14:30:49 -0300

    Configurando store e state com Vuex

---
+++ src/store/modules/addresses.js
@@ -0,0 +1,54 @@
+// aqui serão feitas as solicitações ao Firestore
+
+const state = {
+  addresses: [
+    {
+      id: 3,
+      title: "Shopping Rio Sul",
+      zipCode: "22290-070",
+      city: "Rio de Janeiro",
+      state: "RJ",
+      street: "Avenida Lauro Sodré",
+      neighborhood: "Botafogo",
+      number: "445",
+      complement: "Parte DC01, Shop 401"
+    },
+    {
+      id: 2,
+      title: "Shopping Tijuca",
+      zipCode: "20511-000",
+      city: "Rio de Janeiro",
+      state: "RJ",
+      street: "Avenida Maracanã",
+      neighborhood: "Tijuca",
+      number: "987",
+      complement: "7º piso"
+    },
+    {
+      id: 1,
+      title: "Shopping Downtown",
+      zipCode: "22640-904",
+      city: "Rio de Janeiro",
+      state: "RJ",
+      street: "Avenida das Américas",
+      neighborhood: "Centro",
+      number: "500",
+      complement: "Bloco 6 2º piso"
+    }
+  ]
+};
+
+const getters = {
+  allAddresses: state => state.addresses
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
