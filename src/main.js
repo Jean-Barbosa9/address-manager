b7373fd90ceed2e696fa76c1f98a5acb68156441
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-15 10:29:10 -0300

    Inicializando firebase

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import store from "./store";
 import routes from "./routes.js";
+import firebase from "firebase";
+import variables from "./variables";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({ routes });
+
+firebase.initializeApp(variables.firebaseConfig);
 
 new Vue({
   store,
