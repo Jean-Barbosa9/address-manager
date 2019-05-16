0ef0d2c3acc256797019f2a437c6851c6232971c
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-16 03:01:11 -0300

    Inclusão de novo módulo da store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import addresses from "./modules/addresses";
+import authenicate from "./modules/authenicate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    addresses
+    addresses,
+    authenicate
   }
 });
