ae6036e8e1657c8a46f52d0ce27abf4426da7014
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-16 03:05:00 -0300

    Métodos do CRUD se comunicando com o firestore

--- src/store/modules/addresses.js
+++ src/store/modules/addresses.js
@@ -1,11 +1,12 @@
-import uuid from "uuid";
-
-// aqui serão feitas as solicitações ao Firestore
+// import uuid from "uuid";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const state = {
   alert: {
     type: null,
-    message: ""
+    message: "",
+    timeout: null
   },
   editingAddress: {
     id: null,
@@ -19,41 +20,43 @@ const state = {
     complement: ""
   },
   addresses: [
-    {
-      id: 3,
-      title: "Shopping Rio Sul",
-      zipCode: "22290-070",
-      city: "Rio de Janeiro",
-      state: "RJ",
-      street: "Avenida Lauro Sodré",
-      neighborhood: "Botafogo",
-      number: "445",
-      complement: "Parte DC01, Shop 401"
-    },
-    {
-      id: 2,
-      title: "Shopping Tijuca",
-      zipCode: "20511-000",
-      city: "Rio de Janeiro",
-      state: "RJ",
-      street: "Avenida Maracanã",
-      neighborhood: "Tijuca",
-      number: "987",
-      complement: "7º piso"
-    },
-    {
-      id: 1,
-      title: "Shopping Downtown",
-      zipCode: "22640-904",
-      city: "Rio de Janeiro",
-      state: "RJ",
-      street: "Avenida das Américas",
-      neighborhood: "Centro",
-      number: "500",
-      complement: "Bloco 6 2º piso"
-    }
-  ],
-  teste: ""
+    // {
+    //   id: 3,
+    //   createdBy: "jeanbarbosa9@gmail.com",
+    //   title: "Shopping Rio Sul",
+    //   zipCode: "22290-070",
+    //   city: "Rio de Janeiro",
+    //   state: "RJ",
+    //   street: "Avenida Lauro Sodré",
+    //   neighborhood: "Botafogo",
+    //   number: "445",
+    //   complement: "Parte DC01, Shop 401"
+    // },
+    // {
+    //   id: 2,
+    //   createdBy: "jeanbarbosa9@gmail.com",
+    //   title: "Shopping Tijuca",
+    //   zipCode: "20511-000",
+    //   city: "Rio de Janeiro",
+    //   state: "RJ",
+    //   street: "Avenida Maracanã",
+    //   neighborhood: "Tijuca",
+    //   number: "987",
+    //   complement: "7º piso"
+    // },
+    // {
+    //   id: 1,
+    //   createdBy: "jeanbarbosa9@gmail.com",
+    //   title: "Shopping Downtown",
+    //   zipCode: "22640-904",
+    //   city: "Rio de Janeiro",
+    //   state: "RJ",
+    //   street: "Avenida das Américas",
+    //   neighborhood: "Centro",
+    //   number: "500",
+    //   complement: "Bloco 6 2º piso"
+    // }
+  ]
 };
 
 const getters = {
@@ -63,28 +66,98 @@ const getters = {
 };
 
 const actions = {
-  fetchAddresses({ commit }) {
+  getLocalAddresses({ commit }) {
     const localAddresses = JSON.parse(atob(localStorage.addresses));
     commit("setAddresses", localAddresses);
+  },
+  fetchAddresses({ commit }, user) {
+    const userAddresses = firebase
+      .firestore()
+      .collection("addresses")
+      .where("createdBy", "==", user);
+    userAddresses
+      .orderBy("title")
+      .get()
+      .then(querySnapshot => {
+        const userSycedAddresses = [];
+        querySnapshot.forEach(doc => {
+          const {
+            createdBy,
+            title,
+            zipCode,
+            city,
+            state,
+            street,
+            neighborhood,
+            number,
+            complement
+          } = doc.data();
+
+          userSycedAddresses.push({
+            id: doc.id,
+            createdBy,
+            title,
+            zipCode,
+            city,
+            state,
+            street,
+            neighborhood,
+            number,
+            complement
+          });
+        });
+        commit("setAddresses", userSycedAddresses);
+      });
   },
   willEdit({ commit }, id) {
     commit("setEditAddress", id);
   },
   addAddress({ commit }, address) {
-    address.id = uuid.v4();
+    // address.id = uuid.v4();
+    firebase
+      .firestore()
+      .collection("addresses")
+      .doc()
+      .set(address);
     commit("newAddress", address);
   },
   editAddress({ commit }, payload) {
-    commit("addressChanged", payload);
+    firebase
+      .firestore()
+      .collection("addresses")
+      .doc(payload.id)
+      .set(payload)
+      .then(res => {
+        commit("addressChanged", payload);
+        commit("alert", {
+          type: "success",
+          message: "Endereço atualizado com sucesso!",
+          timeout: 2000
+        });
+      })
+      .catch(error => {
+        commit("alert", {
+          type: "error",
+          message: error,
+          timeout: 5000
+        });
+      });
   },
   deleteAddress({ commit }, id) {
-    commit("addressRemoved", id);
+    firebase
+      .firestore()
+      .collection("addresses")
+      .where("id", "==", id)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          doc.ref.delete();
+        });
+        commit("addressRemoved", id);
+      });
   },
   saveAddress({ commit }, message) {
     commit("localSave", message);
-  },
-  test({ commit }, data) {
-    commit("testing", data);
   }
 };
 
@@ -107,17 +180,20 @@ const mutations = {
     localStorage.setItem("addresses", btoa(JSON.stringify(state.addresses)));
     state.alert = {
       type: "success",
-      message: message
+      message: message,
+      timeout: 2000
     };
+  },
 
+  alert: (state, alert) => {
+    state.alert = alert;
     setTimeout(() => {
       state.alert = {
         type: null,
         message: ""
       };
-    }, 2000);
-  },
-  testing: (state, data) => (state.teste = data)
+    }, alert.timeout);
+  }
 };
 
 export default {
