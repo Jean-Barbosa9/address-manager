82a92aee4947905a9e95a82db548b8f88a754147
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-05-16 21:18:44 -0300

    Incluindo a página que renderizará os endereços compartilhados

---
+++ src/views/Shared.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "Shared",
+  created() {
+    this.fetchSharedAddresses(btoa(this.$route.params.id));
+  },
+  methods: {
+    ...mapActions(["fetchSharedAddresses"])
+  },
+  data() {
+    return {
+      sharedAddresses: []
+    };
+  }
+};
+</script>
+
+<style>
+</style>
